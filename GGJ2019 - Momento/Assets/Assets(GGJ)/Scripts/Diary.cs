ba229cdd8f3226557a823c1f5213acfc97deb15b
LittleRookey <37283117+LittleRookey@users.noreply.github.com>
2019-01-28 17:36:59 -0500

    GGJ
    Good luck1

---
+++ GGJ2019 - Momento/Assets/Assets(GGJ)/Scripts/Diary.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Diary : MonoBehaviour
+{
+    public Text story;
+
+    private string explanations1;
+    private string explanations2;
+
+    private void Start()
+    {
+        explanations1 = "Dear Diary, Today was the third day of our family trip. " +
+            "Sam has always wanted to come to the amusement park, " +
+            "so we finally brought him here. As soon as we entered the park," +
+            " he dashed off with Andrew to line up for the roller coaster ride.";
+        explanations2 = "Isabelle didn’t want to take a ride, " +
+            "so I stayed with her and waited for the kids." +
+            " A staff member then asked us if we wanted to take a family photo." +
+            " We happily accepted his offer.";
+    }
+    private void Update()
+    {
+        
+    }
+
+
+
+}
---
+++ GGJ2019 - Momento/Assets/Assets2(Demo)/Scripts/NPC.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC : MonoBehaviour {
+
+    public string dialogue;
+    public Text dialogueText;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(Input.GetKeyDown(KeyCode.Q)) {
+            if (other.CompareTag("Player"))
+            {
+                Debug.Log(dialogue);
+                dialogueText.text = dialogue;
+            }
+        }
+    }
+
+}
